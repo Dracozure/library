55971e6626a574fdeabbf0503cfeb3ded5d1bda3
Quan <61860115+Dracozure@users.noreply.github.com>
2024-10-13 22:59:15 -0700

    Add book button toggle modal

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const myLibrary = [];
 
-const addBookButton = document.getElementById('.add-book');
+const addBookButton = document.querySelector('.add-book');
+const modal = document.querySelector('.modal');
+
+addBookButton.addEventListener("click", () => {
+    modal.classList.add("active");
+});
 
 function Book(title, author, pages, read) {
     this.title = title;
