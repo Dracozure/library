305f41e2dbc518fa7bb7b69138c52e785a655318
Quan <61860115+Dracozure@users.noreply.github.com>
2024-10-14 23:46:32 -0700

    Centering the modal manually

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const myLibrary = [];
 
 const addBookButton = document.querySelector('.add-book');
 const modal = document.querySelector('.modal');
+let mouseDown = 0;
 
 addBookButton.addEventListener("click", () => {
     modal.classList.add("active");
+});
+
+modal.addEventListener("mousedown", (event) => {
+    mouseDown = 1;
 });
 
 function Book(title, author, pages, read) {
