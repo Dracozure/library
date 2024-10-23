fa7f6cd264b48890cba7c1bad35a1cee28b0f979
Quan <61860115+Dracozure@users.noreply.github.com>
2024-10-22 18:03:08 -0700

    Change how book card is created with library
    Created a library object to store book card
    elements.

--- script.js
+++ script.js
@@ -1,52 +1,25 @@
-const myLibrary = [];
-
 const addBookButton = document.querySelector('.add-book');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const form = document.querySelector('form');
 const bookGrid = document.querySelector('.book-grid');
-let mouseDown = 0;
+const library = {
+    books: [],
+    assignBookElementIndex: function(bookCardElement) {
+        const index = this.books.length
+
+        bookCardElement.setAttribute('data', index);
+    },
+    addBookElement: function(bookCardElement) {
+        this.books.push(bookCardElement);
+    }
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
-
-Book.prototype.createBookCardElement = function(title, author,pages, read) {
-    const newBookCardElement = document.createElement('div');
-    const titleElement = document.createElement('h4');
-    const authorElement = document.createElement('h4');
-    const pagesElement = document.createElement('h4');
-    const buttonGroup = document.createElement('div');
-    const readElement = document.createElement('button');
-    const removeElement = document.createElement('button');
-    const readValue = (read === 'on') ? 'Read' : 'Unread';
-
-    titleElement.textContent = title;
-    authorElement.textContent = author;
-    pagesElement.textContent = pages;
-    readElement.textContent = readValue;
-    removeElement.textContent = 'Remove';
-
-    newBookCard.classList.add('book-card');
-    titleElement.classList.add('book-title');
-    authorElement.classList.add('book-author');
-    pagesElement.classList.add('pages');
-    buttonGroup.classList.add('button-group');
-    readElement.classList.add(readValue.toLowerCase());
-    removeElement.classList.add('remove');
-
-    buttonGroup.appendChild(readElement);
-    buttonGroup.appendChild(removeElement);
-
-    newBookCard.appendChild(titleElement);
-    newBookCard.appendChild(authorElement);
-    newBookCard.appendChild(pagesElement);
-    newBookCard.appendChild(buttonGroup);
-
-    return newBookCardElement;
 }
 
 addBookButton.addEventListener('click', () => {
@@ -62,9 +35,46 @@ form.addEventListener('submit', () => {
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const read = document.getElementById('read').value;
-    const bookCard = createBookCard(title, author, pages, read);
+    const bookCardElement = createBookCardElement(new Book(title, author, pages, read));
 
-    bookGrid.appendChild(bookCard);
+    library.assignBookElementIndex(bookCardElement);
+    library.addBookElement(bookCardElement);
+    bookGrid.appendChild(bookCardElement);
     modal.classList.remove('active');
 });
+
+createBookCardElement = function(book) {
+    const bookCardElement = document.createElement('div');
+    const titleElement = document.createElement('h4');
+    const authorElement = document.createElement('h4');
+    const pagesElement = document.createElement('h4');
+    const buttonGroup = document.createElement('div');
+    const readElement = document.createElement('button');
+    const removeElement = document.createElement('button');
+    const readValue = (book.read === 'on') ? 'Read' : 'Unread';
+
+    titleElement.textContent = book.title;
+    authorElement.textContent = book.author;
+    pagesElement.textContent = book.pages;
+    readElement.textContent = readValue;
+    removeElement.textContent = 'Remove';
+
+    bookCardElement.classList.add('book-card');
+    titleElement.classList.add('book-title');
+    authorElement.classList.add('book-author');
+    pagesElement.classList.add('pages');
+    buttonGroup.classList.add('button-group');
+    readElement.classList.add(readValue.toLowerCase());
+    removeElement.classList.add('remove');
+
+    buttonGroup.appendChild(readElement);
+    buttonGroup.appendChild(removeElement);
+
+    bookCardElement.appendChild(titleElement);
+    bookCardElement.appendChild(authorElement);
+    bookCardElement.appendChild(pagesElement);
+    bookCardElement.appendChild(buttonGroup);
+
+    return bookCardElement;
+}
 
